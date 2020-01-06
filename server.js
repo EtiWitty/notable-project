1568c8100c7f84cd96ff03b40bdf387ffc159c40
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-01-06 13:32:00 -0800

    implemented err handling middleware

--- server.js
+++ server.js
@@ -14,3 +14,13 @@ const PORT = parseInt(process.env.PORT, 10) || 3000;
 app.listen (PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
 });
+
+//error-handler middleware
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send({
+		status: 500,
+		message:err.message,
+		body: {}
+	});
+});
