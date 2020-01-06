956e814f3dde8b2bade8d7974fc6308be068c9ec
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-01-06 13:20:58 -0800

    initiated GET method

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ dotEnv.config();
 
 const app = express();
 
+app.get('/',(req, res, next) => {
+	res.send('Hello to Notable');
+});
+
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 app.listen (PORT, () => {
